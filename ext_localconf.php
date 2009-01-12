4d81d79d80342ad898c71dc09f56f317f4513b4e
sorenmalling <sorenmalling@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-12 20:03:54 +0000

    Initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fedashboard/trunk@15622 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+ 	die ('Access denied.');
+}
+
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_fedashboard_pi1.php', '_pi1', 'list_type', 0);
+
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_fedashboard_widgets=1
+');
+?>
