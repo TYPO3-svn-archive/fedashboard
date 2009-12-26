c95e482a7ac60622d624e7fd3fa5b2b4fe9181f0
sorenmalling <sorenmalling@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-12-26 23:14:40 +0000

    initial import from kickstarter
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fedashboard/trunk@28068 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "fedashboard".
+#
+# Auto generated 22-12-2009 14:56
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend dashboard',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Soren Malling',
+	'author_email' => 'soren.malling@gmail.com',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"52d3";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"760c";s:14:"ext_tables.php";s:4:"a06d";s:13:"locallang.xml";s:4:"72f4";s:16:"locallang_db.xml";s:4:"0c9c";s:19:"doc/wizard_form.dat";s:4:"cbf1";s:20:"doc/wizard_form.html";s:4:"decd";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:32:"pi1/class.tx_fedashboard_pi1.php";s:4:"5e26";s:40:"pi1/class.tx_fedashboard_pi1_wizicon.php";s:4:"b4ab";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6b82";s:32:"static/fedashboard/constants.txt";s:4:"d41d";s:28:"static/fedashboard/setup.txt";s:4:"d41d";}',
+);
+
+?>
